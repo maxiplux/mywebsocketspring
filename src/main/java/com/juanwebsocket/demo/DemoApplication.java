9a46cef3dd6cd81fab06f91cd54b128f30df1e19
Juan Francisco Mosquera <maxiplux@gmail.com>
2019-03-20 10:45:31 -0500

    Initial commit

---
+++ src/main/java/com/juanwebsocket/demo/DemoApplication.java
@@ -0,0 +1,31 @@
+package com.juanwebsocket.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@SpringBootApplication
+@EnableWebSocketMessageBroker
+public class DemoApplication  implements WebSocketMessageBrokerConfigurer {
+
+    public static void main(String[] args)
+
+    {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");
+    }
+
+}
